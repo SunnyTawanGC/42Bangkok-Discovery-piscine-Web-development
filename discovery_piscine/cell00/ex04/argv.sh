0fe2b0e591ddb4173c8bfe1e4f60f262fec848eb
SunnyTawanGC <tawanpraporn@gmail.com>
2026-02-03 10:50:23 +0700

    ex04

---
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    if [ ! -z "$1" ]; then
+        echo "$1"
+    fi
+    if [ ! -z "$2" ]; then
+        echo "$2"
+    fi
+    if [ ! -z "$3" ]; then
+        echo "$3"
+    fi
+fi
